001fe7191644f103e1afa2d397fe169e05f136ca
zedyautja <richhalfpenny@gmail.com>
2014-08-22 16:54:51 +0100

    Example code for R programming assignment 2

---
+++ example.R
@@ -0,0 +1,29 @@
+# This is the code from box one of the  assignment page example exactly as written
+
+makeVector <- function(x = numeric()) {   
+        m <- NULL
+        set <- function(y) {
+                x <<- y                 
+                m <<- NULL
+        }
+        get <- function() x
+        setmean <- function(mean) m <<- mean
+        getmean <- function() m
+        list(set = set, get = get,
+             setmean = setmean,
+             getmean = getmean)
+}
+
+# This is the code from box 2 of the assignment page example exactly as written
+
+cachemean <- function(x, ...) {
+        m <- x$getmean()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- mean(data, ...)
+        x$setmean(m)
+        m
+}
