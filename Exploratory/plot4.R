4c41296f21425b0435978fe50c63cbfb2f8044e7
zedyautja <richhalfpenny@gmail.com>
2014-09-11 11:28:22 +0100

    Project 1 code and png's

--- Exploratory/plot4.R
+++ Exploratory/plot4.R
@@ -1,6 +1,6 @@
 #setwd("~/GitHub/R-working/Coursera stuff/Exploratory data analysis")
 
-file <- "./data/household_power_consumption.txt"   # set the file location in local system
+file <- "household_power_consumption.txt"   # set the file location in local system
 
 hpc <- read.csv(file, sep=";", na.strings="?", stringsAsFactors=FALSE) #get the file and set the ? as na value
 
@@ -23,7 +23,7 @@ with (hpc2, {
       points(DateTime, Sub_metering_2, type = "l", col = "red") # add second column points and set to red colour line
       points(DateTime, Sub_metering_3, type = "l", col = "blue") # add third column points and set to blue colour line
       legend("topright", xjust = 1, bty = "n", col = c("black", "red", "blue"), lwd = 2, cex = 0.8, legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3")) # create top right legend
-  plot(DateTime, Global_reactive_power,  type ="h", xlab = "datetime") # bottom right
+  plot(DateTime, Global_reactive_power,  type ="l", xlab = "datetime") # bottom right
 })
 
 dev.copy(png, file = "plot4.png", width = 480, height = 480, units = "px")  #copy the plot to png file in working dir and set its size
